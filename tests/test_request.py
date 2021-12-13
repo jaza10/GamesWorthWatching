938d658b98a256562981be667850a43ecab2fd49
Jan Zawadzki <jaza10@me.com>
2021-12-13 22:37:17 +0100

    fixed test case

--- tests/test_request.py
+++ tests/test_request.py
@@ -4,8 +4,12 @@ from game_app import app
 
 NBA_url = "http://data.nba.net/10s/prod/v1/20211211/scoreboard.json"
 
+# TODO: Add test for no games the previous night
+# TODO: Check for catching results of multiple close games
+# TODO: Check for catching no results on a day with no close games, e.g. 09 Dec 21
+
 class TestRequests(unittest.TestCase):
     def test_check_close_game(self):
 
         response = app.check_close_game(NBA_url)
-        self.assertEqual(response, 'Games worth watching on 2021-12-11: ORL : LAC, \n')
+        self.assertEqual(response, 'Games worth watching on 20211212: ORL : LAC, \n')
