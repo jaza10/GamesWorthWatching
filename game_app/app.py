0690d5fad8e0d937a95f77bf8e9e5f65ce0b58eb
Jan Zawadzki <jaza10@me.com>
2021-12-18 21:55:45 +0100

    added html and adjusted result depiction

--- game_app/app.py
+++ game_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 from datetime import date, timedelta
 
@@ -9,15 +9,16 @@ NBA_url_temp = "http://data.nba.net/10s/prod/v1/{date}/scoreboard.json"
 @app.route("/")
 def index():
 
+  # Making sure that right date is fetched each time page is requested, and results are not cached
   yesterday = date.today() - timedelta(1)
   yesterday_string = yesterday.strftime("%Y%m%d")
   NBA_url = NBA_url_temp.format(date = yesterday_string)
-  return check_close_game(NBA_url, yesterday_string)
+  return render_template("index.html", date = yesterday, results = check_close_game(NBA_url))
 
-def check_close_game(url, date):
+def check_close_game(url):
   response = requests.get(url)
   numGames = response.json().get("numGames")
-  result = "Games worth watching on " + date + ": " 
+  result = []
   # TODO: include checks for NoneTypes and robust API requests
   # TODO: insert response for 0 games worth watching
   for i in range(numGames):
@@ -30,7 +31,6 @@ def check_close_game(url, date):
     if score_difference <= 6:
       vteam_name = response.json().get("games")[i]["vTeam"]["triCode"]
       hteam_name = response.json().get("games")[i]["hTeam"]["triCode"]
-      result += vteam_name + " : " + hteam_name +", \n"
+      result.append(vteam_name + " : " + hteam_name)
           
   return result
-
