8819bf47f97acaa1d0ac2323583c9fc2d0014077
Jan Zawadzki <jaza10@me.com>
2021-12-29 21:50:19 +0100

    adjusted response of close games check to add URL link to games worth watching site. Added a test, new JSON test file, and adjusted HTML to inlcude links to games.

--- tests/test_request.py
+++ tests/test_request.py
@@ -12,25 +12,27 @@ NBA_url = "http://data.nba.net/10s/prod/v1/20211211/scoreboard.json"
 # TODO: Check for catching no results on a day with no close games, e.g. 09 Dec 21
 
 class TestRequests(unittest.TestCase):
-    #def test_check_close_game(self):
-
-        # Sends API request, pot remove later
-        #response = app.check_close_game(NBA_url)
-        #self.assertEqual(response, ['ORL : LAC'])
 
     def test_no_error_on_postponed_games(self):
+        testJSON = open('tests/resources/PostponedGameAndNoCloseGame.json')
+        data = json.load(testJSON)
+        testJSON.close()
+        response = app.check_close_game(data)
+        self.assertEqual(response, {})
+
+    def test_no_error_on_postponed_games_and_no_close_game(self):
         testJSON = open('tests/resources/PostponedGameAndOT.json')
         data = json.load(testJSON)
         testJSON.close()
         response = app.check_close_game(data)
-        self.assertEqual(response, ['TOR : BKN', 'PHX : POR'])
+        self.assertEqual(response, {'game0': {'vteam_name': 'TOR', 'hteam_name': 'BKN', 'game_summary_url': 'https://www.nba.com/game/TOR-vs-BKN-0022100413?watch'}, 'game3': {'vteam_name': 'PHX', 'hteam_name': 'POR', 'game_summary_url': 'https://www.nba.com/game/PHX-vs-POR-0022100416?watch'}})
 
     def test_correctly_identify_no_games_worth_watching(self):
         testJSON = open('tests/resources/NoGameWorthWatching.json')
         data = json.load(testJSON)
         testJSON.close()
         response = app.check_close_game(data)
-        self.assertEqual(response, [])
+        self.assertEqual(response, {})
 
 
 
